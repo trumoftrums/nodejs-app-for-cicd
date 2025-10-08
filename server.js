4de2cfa1e073969c79f5d450d8a6ef35ca7b4e41
baonghiem <nghiembaospkit@gmail.com>
2025-10-08 15:17:12 +0700

    test wfl 04

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const os = require('os');
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send(`🚀 Hello from Node.js app running on Kubernetes update time n! on pod: ${process.env.HOSTNAME || os.hostname()}`);
+  res.send(`🚀 Hello from Node.js app running on Kubernetes version 888! on pod: ${process.env.HOSTNAME || os.hostname()}`);
 });
 
 app.listen(PORT, () => {
